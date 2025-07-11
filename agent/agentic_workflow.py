7ddbf7910f7e930d30c4e0757a8b77bc2657f630
KD <itzkushan@gmail.com>
2025-07-11 11:24:37 +0530

    code updates

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -5,15 +5,26 @@ from langgraph.prebuilt import ToolNode, tools_condition
 
 from tools.weather_info_tool import WeatherInfoTool
 from tools.place_search_tool import PlaceSearchTool
-from tools.calculator_tool import CalculatorTool
+from tools.expense_calculator_tool import CalculatorTool
 from tools.currency_conversion_tool import CurrencyConverterTool
 
 class GraphBuilder():
-    def __init__(self, graph):
-        self.tools=[
-            #WeatherInfoTool(),
-
-        ]
+    def __init__(self, model_provider:str = "groq"):
+        self.model_loader = ModelLoader(model_provider=model_provider)
+        self.llm = self.model_loader.load_llm()
+        self.tools = []            
+        self.weather_tools= WeatherInfoTool()
+        self.place_search_tools= PlaceSearchTool()
+        self.calculator_tools= CalculatorTool()
+        self.currency_converter_tools= CurrencyConverterTool()
+        self.tools.extend([*self.weather_tool.weather_tool_list,
+                               *self.place_search_tools.place_search_tool_list
+                               *self.calculator_tools.calculator_tool_list
+                               *self.currency_converter_tools.currency_converter_tool_list])
+        self.llm_with_tools = self.llm.bind_tools(tools=self.tools)
+        self.graph = None
+        self.system_prompt = SYSTEM_PROMPT
+        
         self.system_prompt= SYSTEM_PROMPT
 
     def agent_function(self, state: MessageState):
---
+++ tools/weather_info_tool.py
@@ -0,0 +1,40 @@
+import os
+from typing import List, Any, Dict, Optional
+from dotenv import load_dotenv
+
+from langchain.tools import tool
+from langchain_groq import ChatGroq
+
+class WeatherInfoTool:
+    def __init__(self):
+        load_dotenv()
+        self.api_key = os.environ.get("OPENWEATHER_API_KEY")
+        self.weather_service = WeatherForecastTool(self.api_key)
+        self.weather_tool_list = self._setup_tools()
+
+
+    def _setup_tools(self)-> List:
+        @tool
+        def get_current_weather(city: str)-> str:
+            weather_data = self.weather_service.get_current_weather(city)
+            if weather_data:
+                temp = weather_data.get('main',{}).get('temp','N/A')
+                desc = weather_data.get('weather',[{}])[0].get('description','N/A')
+                return f"Current weather in {city}:{temp},{desc}"
+            return f"couldn't fetch weather for {city}"
+        
+        @tool
+        def get_weather_forecast(city:str)->str:
+            forecast_data = self.weather_service.get_forecast_weather(city)
+            if forecast_data and 'list' in forecast_data:
+                forecast_summary=[]
+                for i in range(len(forecast_data['list'])):
+                    item = forecast_data['list'][i]
+                    date = item['dt_txt'].split('')[0]
+                    temp = item['main']['temp']
+                    desc = item['weather'][0]['description']
+                    forecast_summary.append(f"{date}:{temp}.{desc}")
+                return f"weather forecast for {city}:\n"+"\n".join(forecast_summary)
+            return f"couldn't fetch forecast for {city}"
+        return[get_current_weather,get_weather_forecast]
+     
